4e16fe263ed44e4a2365c2502b2c1e4b4471d93f
Jacob A. Clark <77306976+Triv-Code@users.noreply.github.com>
2021-02-21 23:54:19 -0500

    Added Comments - Some Screen Sizing

--- js/app.js
+++ js/app.js
@@ -1,3 +1,6 @@
+// This is "a solution", but I'm not sure it is the best solution. 
+// Is this how you would go about solving the problem? 
+
 const carouselSlide = document.querySelector('.carousel-slide');
 const carouselImages = document.querySelectorAll('.carousel-slide img');
 
@@ -12,21 +15,21 @@ const size = carouselImages[0].clientWidth;
 // carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
 
 //Button Listeners
-
+            // ??? --- Can I add Arrow Keys < / > to the EventListener? --- ??? 
 nextBtn.addEventListener('click', () => {
     if (counter >= carouselImages.length - 1) return;
     carouselSlide.style.transition = "transform 0.4s ease-in-out";
     counter++;
     carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
 });
-
+            // ??? --- Can I add Arrow Keys < / > to the EventListener --- ??? 
 prevBtn.addEventListener('click', () => {
     if (counter <= 0) return;
     carouselSlide.style.transition = "transform 0.4s ease-in-out";
     counter--;
     carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
 });
-
+            // ??? --- Why doesn't this loop the photos back? --- ???
 carouselSlide.addEventListener('transitioned', () => {
     if (carouselImages[counter].id === 'lastClone') {
         carouselSlide.style.transition = "none";
