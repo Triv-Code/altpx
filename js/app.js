30c9b5c27ac88c9b3a896696dab355794862f936
Jacob A. Clark <77306976+Triv-Code@users.noreply.github.com>
2021-02-21 20:03:35 -0500

    Gallery stops

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@ const prevBtn = document.querySelector('#prevBtn');
 const nextBtn = document.querySelector('#nextBtn');
 
 //Counter
-let counter = 1;
+let counter = 0;
 const size = carouselImages[0].clientWidth;
 
 carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
@@ -14,7 +14,7 @@ carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
 //Button Listeners
 
 nextBtn.addEventListener('click', () => {
-    if(counter >= carouselImages.length -1) return;
+    if (counter >= carouselImages.length - 1) return;
     carouselSlide.style.transition = "transform 0.4s ease-in-out";
     counter++;
     carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
@@ -28,15 +28,15 @@ prevBtn.addEventListener('click', () => {
 });
 
 carouselSlide.addEventListener('transitioned', () => {
-    if (carouselImages[counter] /*.id === 'lastClone'*/) {
+    if (carouselImages[counter].id === 'lastClone') {
         carouselSlide.style.transition = "none";
-        counter = carouselImages.length - 2 ;
+        counter = carouselImages.length - 2;
         carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
     }
-    /*if (carouselImages[counter] .id === 'firstClone') {
+    if (carouselImages[counter].id === 'firstClone') {
         carouselSlide.style.transition = "none";
         counter = carouselImages.length - counter;
         carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
-    }*/
+    }
 
 });
