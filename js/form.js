3c067a6731b48b2b5a7c9137ce38f8f0efc67cb0
Jacob A. Clark <77306976+Triv-Code@users.noreply.github.com>
2021-03-02 01:28:01 -0500

    Form Validation (Failed) + Notes

--- js/form.js
+++ js/form.js
@@ -33,3 +33,35 @@ form.addEventListener('submit', (e) => {
     //     form.remove();
     // }
 })
+
+// ------------------ Old Code, Just saving" 
+// function isValidEmail(email) {
+//     const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+//     return re.test(String(email).toLowerCase());
+// }
+
+// const fname = document.getElementById("fname");
+// const lname = document.getElementById("lname");
+// const email = document.getElementById("email");
+// const subject = document.getElementById("subject");
+
+// const form = document.querySelector('form');
+// const nameInput = document.getElementsByName('name');
+
+// let isFormValid = false;
+
+// const f_validateInputs = () => {
+//     isFormValid = true;
+//     if (!nameInput.value) {
+//         document.getElementById('name_error').classList.remove("hidden");
+//         isFormValid = false;
+//     }
+// };
+
+// form.addEventListener('submit', (e) => {
+//     e.preventDefault();
+//     f_validateInputs();
+//     // if (isformValid = true) {
+//     //     form.remove();
+//     // }
+// })
