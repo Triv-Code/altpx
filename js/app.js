598ca675e76245eec6e574dd5693d3766e0239bf
Jacob A. Clark <77306976+Triv-Code@users.noreply.github.com>
2021-03-05 16:38:25 -0500

    Carousel JS cleaned up, Image Scaling Behavior Change

--- js/app.js
+++ js/app.js
@@ -1,32 +1,41 @@
+// --- Selectors for Images
 const carouselSlide = document.querySelector('.carousel-slide');
 const carouselImages = document.querySelectorAll('.carousel-slide img');
 
-//Buttons
+// --- Selectors for Buttons
 const prevBtn = document.querySelector('#prevBtn');
 const nextBtn = document.querySelector('#nextBtn');
 
-//Counter
+// --- Counter & Size (How many pixels to skip forward.)
 let counter = 0;
 const size = carouselImages[0].clientWidth;
 
-carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
-
-//Button Listeners
-            // ??? --- Can I add Arrow Keys < / > to the EventListener? --- ??? 
-nextBtn.addEventListener('click', () => {
+// --- Next / Previous Picture Movement
+function nextPicture(){
     if (counter >= carouselImages.length - 1) return;
     carouselSlide.style.transition = "transform 0.4s ease-in-out";
     counter++;
     carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
-});
-            // ??? --- Can I add Arrow Keys < / > to the EventListener --- ??? 
-prevBtn.addEventListener('click', () => {
+}
+
+function prevPicture(){
     if (counter <= 0) return;
     carouselSlide.style.transition = "transform 0.4s ease-in-out";
     counter--;
     carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
-});
-            // ??? --- Auto transitioning on Clone imgs when it should hold. Needs Debugged --- ???
+}
+
+// --- Event Listeners for Next / Previous Picture Movement
+nextBtn.addEventListener('click', nextPicture);
+prevBtn.addEventListener('click', prevPicture);
+
+function test(event) {
+var x = event.key;
+if (x == "a") {
+    console.log(peanut);
+}
+}
+// ??? --- Auto transitioning on Clone imgs when it should hold. Needs Debugged --- ???
 // carouselSlide.addEventListener('transitionend', () => {
 //     if (carouselImages[counter].id === 'lastClone') {
 //         carouselSlide.style.transition = "none";
--- js/form.js
+++ js/form.js
@@ -1,22 +1,22 @@
-const comment_name = document.getElementsByName("name");
-const email = document.getElementById("email");
-const subject = document.getElementById("subject");
-const form = document.querySelector('form');
-let isValid = false;
+// const comment_name = document.getElementsByName("name");
+// const email = document.getElementById("email");
+// const subject = document.getElementById("subject");
+// const form = document.querySelector('form');
+// let isValid = false;
 
-function isValidEmail(email) {
-    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
-}
+// function isValidEmail(email) {
+//     const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+//     return re.test(String(email).toLowerCase());
+// }
 
 
-const validName = () => {
-    isValid = true;
-    if (!comment_name.value) {
-        alert("Please tell us your name.");
-        isValid = false;
-    }
-}
+// const validName = () => {
+//     if (!comment_name.value) {
+//         alert("Please tell us your name.");
+//     } else {
+//         console.log("work?");
+//     }
+// }
 
 // const validEmail = () => {
 //     isValid = true;
@@ -26,13 +26,13 @@ const validName = () => {
 //     }
 // }
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    validName();
-    // if (isformValid = true) {
-    //     form.remove();
-    // }
-})
+// form.addEventListener('submit', (e) => {
+//     e.preventDefault();
+//     validName();
+//     if (isformValid = true) {
+//         form.remove();
+//     }
+// })
 
 // ------------------ Old Code, Just saving" 
 // function isValidEmail(email) {
@@ -40,7 +40,7 @@ form.addEventListener('submit', (e) => {
 //     return re.test(String(email).toLowerCase());
 // }
 
-// const fname = document.getElementById("fname");
+// const name = document.getElementById("name");
 // const lname = document.getElementById("lname");
 // const email = document.getElementById("email");
 // const subject = document.getElementById("subject");
@@ -50,7 +50,7 @@ form.addEventListener('submit', (e) => {
 
 // let isFormValid = false;
 
-// const f_validateInputs = () => {
+// const validateInputs = () => {
 //     isFormValid = true;
 //     if (!nameInput.value) {
 //         document.getElementById('name_error').classList.remove("hidden");
@@ -60,8 +60,33 @@ form.addEventListener('submit', (e) => {
 
 // form.addEventListener('submit', (e) => {
 //     e.preventDefault();
-//     f_validateInputs();
+//     validateInputs();
 //     // if (isformValid = true) {
 //     //     form.remove();
 //     // }
 // })
+
+const form = document.querySelector('form');
+const fname = document.getElementById('fname');
+const lname = document.getElementById('lname');
+const email = document.getElementById('email');
+const message = document.getElementById('subject');
+const error_msg = document.getElementsByClassName('error_hint');
+
+const validateInputs = () => {
+    // isFormValid = true;
+    if (!error_msg.value) {
+        document.getElementById('name_error').classList.remove("hidden");
+        // isFormValid = false;
+    }
+};
+
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+})
+
+
+// Instead of trying to add/remove a class, why not set
+// display to none and then use javascript to change it
+// if the validation is false?
